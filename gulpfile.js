fd42f97ef097d6cb6779c5d0aa072efe587fb8ba
Charles Kouadio <kcharly_91@hotmail.com>
2016-03-04 01:40:53 +0000

    maj gupfile.js v1.0.1

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,40 @@
-//  Require Modules
+// Configuration variables
+var config = {
+    buildFilesFoldersRemove: [
+        'build/scss',
+        'build/js/!(*.min.js)',
+        'build/bower_components'
+    ],
+    jsFilesCompress: [
+        'app/bower_components/jquery/dist/jquery.js',
+        'app/js/**/*.js',
+        '!app/js/**/*.min.js'
+    ]
+};
+
+//  Required Modules
 var gulp = require('gulp'),
     browserSync = require('browser-sync'),
     reload = browserSync.reload,
-    uglify = require('gulp-uglify'),
-    sass = require('gulp-sass'),
+    del = require('del'),
+    autoprefixer = require('gulp-autoprefixer'),
     rename = require('gulp-rename'),
-    autoprefixer = require('gulp-autoprefixer')
-    del = require('del');
+    sass = require('gulp-sass'),
+    uglify = require('gulp-uglify');
 
-//Tasks
+// Log Errors - simple function to remplace the use of gulp-plumber
+function errorlog(err) {
+    console.log(err.message);
+    this.emit('end');
+}
+
+//General Tasks
 //Compress Javascript files
 gulp.task('compress-js', function() {
-    return gulp.src(['app/js/**/*.js', '!app/js/**/*.min.js'])
-    //.pipe(plumber())
-    .pipe(rename({suffix:'.min'}))
+    return gulp.src(config.jsFilesCompress)
     .pipe(uglify())
+    .on('error', errorlog)
+    .pipe(rename({suffix:'.min'}))
     .pipe(gulp.dest('app/js'))
     .pipe(reload({stream:true}));
 });
@@ -22,9 +42,12 @@ gulp.task('compress-js', function() {
 //Compile Sass into Css
 gulp.task('sass', function() {
     return gulp.src('app/scss/style.scss')
-    //.pipe(plumber())
-    .pipe(sass().on('error', sass.logError))
-    //uglify css
+    .pipe(sass({outputStyle: 'compressed'}))
+    .on('error', errorlog)
+    .pipe(autoprefixer({
+        browsers: ['last 3 versions'],
+        cascade: false
+    }))
     .pipe(gulp.dest('app/css'))
     .pipe(reload({stream:true}));
 });
@@ -35,7 +58,8 @@ gulp.task('html', function() {
     .pipe(reload({stream:true}));
 });
 
-//Sync the browser
+//Syncronisations Tasks
+//Sync the browser during development
 gulp.task('browserSync', function() {
     browserSync({
         server: {
@@ -44,46 +68,44 @@ gulp.task('browserSync', function() {
     });
 });
 
+// //Show Build Version in browser
+// gulp.task('build-serve', function() {
+//     browserSync({
+//         server: {
+//             baseDir: 'build'
+//         }
+//     });
+// });
+
+// //Build task
+// //Clean Build directory if it exists
+// gulp.task('build-clean', function(cb) {
+//     del([
+//         'build/**'
+//     ], cb);
+// });
+//
+// //Create Build directory
+// gulp.task('build-copy', ['build-clean'], function() {
+//     return gulp.src('app/**/*')
+//     .pipe(gulp.dest('build/'));
+// });
+//
+// //Remove unwanted build files
+// gulp.task('build-remove', ['build-copy'], function(cb) {
+//     del(config.buildFilesFoldersRemove, cb);
+// });
+//
+// //Build
+// gulp.task('build', ['build-copy', 'build-remove']);
+
+
 //Watch task
-gulp.task('watch', ['browserSync'], function() {
-    gulp.watch('app/**/*.html', ['html']);
+gulp.task('watch', function() {
     gulp.watch('app/js/**/*.js', ['compress-js']);
     gulp.watch('app/scss/**/*.scss', ['sass']);
+    gulp.watch('app/**/*.html', ['html']);
 });
 
 //Default task
-gulp.task('default', ['watch']);
-
-//Build task
-//Clean Build directory if it exists
-gulp.task('build-clean', function(cb) {
-    del([
-        'build/**'
-    ], cb);
-});
-
-//Create Build directory
-gulp.task('build-copy', ['build-clean'], function() {
-    return gulp.src('app/**/*')
-    .pipe(gulp.dest('build'));
-});
-
-//Remove unwanted build files
-gulp.task('build-remove', ['build-copy'], function(cb) {
-    del([
-        'build/scss',
-        'build/js/!(*.min.js)'
-    ], cb);
-});
-
-//Build
-gulp.task('build', ['build-copy', 'build-remove']);
-
-//Show Build Version
-gulp.task('build-serve', function() {
-    browserSync({
-        server: {
-            baseDir: 'build'
-        }
-    });
-});
+gulp.task('default', ['compress-js', 'sass', 'html', 'browserSync', 'watch']);
